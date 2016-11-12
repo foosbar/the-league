11aeed095aa924625ec4ce59b299185e4f555eeb
Kevin Kelley <dev@foos-bar.com>
2016-11-12 00:45:04 -0500

    Added catch all route for HTML requests and added to the teams API

--- app.js
+++ app.js
@@ -33,6 +33,11 @@ app.use('/players', players);
 app.use('/seasons', seasons);
 app.use('/teams', teams);
 
+
+app.get('/*', function(req, res){
+  res.render('index');
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- routes/teams.js
+++ routes/teams.js
@@ -3,12 +3,28 @@ var router = express.Router();
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('theleague.db');
 
+var teamLinks = function(teamId, req) {
+    var teamRef = '/teams/' + teamId;
+    return [{
+        'rel': 'self',
+        'href': req.linkto(teamRef)
+    },{
+        'rel': 'players',
+        'href': req.linkto(teamRef + '/players')
+    },{
+        'rel': 'seasons',
+        'href': req.linkto(teamRef + '/seasons')
+    },{
+        'rel': 'current-season',
+        'href': req.linkto(teamRef + '/seasons/current')
+    }];
+}
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-
     res.format({
         html: function() {
-            res.render('index');
+            next();
         },
 
         json: function() {
@@ -16,7 +32,7 @@ router.get('/', function(req, res, next) {
             db.each("SELECT * FROM team", function(err, row) {
                 console.log('ROW', row);
                 if(row)
-                    row.players = req.linkto('/teams/' + row.id + '/players');
+                    row._links = teamLinks(row.id, req);
                 teams.push(row);
             }, function(err, num) {
                 if(err) {
@@ -29,21 +45,44 @@ router.get('/', function(req, res, next) {
     });
 });
 
-router.get('/:teamId', function(req, res, next) {
+router.post('/', function(req, res, next) {
 
-    var teamId = req.params.teamId;
+    const name = req.body.name;
 
-    db.get("SELECT * FROM team where id = $id", { $id: teamId }, function(err, row) {
-        console.log('ERROR', err);
+    db.run("INSERT INTO team(name) VALUES ($name)", {
+        $name: name
+    }, function(err) {
         if(err) {
-            res.sendStatus(500, err);
-            return;
-        } else if(row) {
-            row.players = req.linkto('/teams/' + row.id + '/players');
-            res.json(row);
-            return;
+            res.sendStatus(500);
         } else {
-            res.sendStatus(404);
+            res.header('Location', req.linkto('/teams/' + this.lastID));
+            res.sendStatus(303);
+        }
+    });
+});
+
+router.get('/:teamId', function(req, res, next) {
+    res.format({
+        html: function() {
+            next();
+        },
+
+        json: function() {
+            var teamId = req.params.teamId;
+
+            db.get("SELECT * FROM team where id = $id", {$id: teamId}, function (err, row) {
+                console.log('ERROR', err);
+                if (err) {
+                    res.sendStatus(500, err);
+                    return;
+                } else if (row) {
+                    row._links = teamLinks(row.id, req);
+                    res.json(row);
+                    return;
+                } else {
+                    res.sendStatus(404);
+                }
+            });
         }
     });
 });
@@ -51,23 +90,32 @@ router.get('/:teamId', function(req, res, next) {
 router.get('/:teamId/players', function(req, res, next) {
 
     var teamId = req.params.teamId;
-    var players = [];
-    db.each("select player.*, team_player.is_captain from player left join team_player on player.id = team_player.player_id where team_player.team_id = $teamId", { $teamId: teamId }, function(err, row) {
-        if(row) {
-            var links = [{
-                rel: 'player',
-                href: req.linkto('/players/' + row.id)
-            }];
-            row.links = links;
 
-            // TODO add in links
-            players.push(row);
-        }
-    }, function(err, num) {
-        if(err) {
-            res.sendStatus(500, err);
-        } else {
-            res.json(players);
+    res.format({
+        html: function() {
+            next();
+        },
+
+        json: function() {
+            var players = [];
+            db.each("select player.*, team_player.is_captain from player left join team_player on player.id = team_player.player_id where team_player.team_id = $teamId", { $teamId: teamId }, function(err, row) {
+                if(row) {
+                    var links = [{
+                        rel: 'player',
+                        href: req.linkto('/players/' + row.id)
+                    }];
+                    row.links = links;
+
+                    // TODO add in links
+                    players.push(row);
+                }
+            }, function(err, num) {
+                if(err) {
+                    res.sendStatus(500, err);
+                } else {
+                    res.json(players);
+                }
+            });
         }
     });
 });
